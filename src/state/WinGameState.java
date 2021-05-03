d17d31e1be813673af54eae5de0d636ce9e87ba2
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-05-03 15:42:23 +0700

    Update WinGameState.java

--- src/state/WinGameState.java
+++ src/state/WinGameState.java
@@ -8,23 +8,16 @@ import main.Handler;
 
 public class WinGameState extends State {
 
-    private String[] options;
 
     private static final String WIN_GAME = "You Win!";
-    private static final String CONTINUE = "Press Enter to continue!";
 
     public WinGameState(Handler handler) {
         super(handler);
-        this.options = new String[] {WIN_GAME, CONTINUE};
     }
 
     @Override
     public void tick() {
         
-        if (handler.getKeyManager().enter) {
-
-            State.setState(handler.getGame().getMainMenuState());
-        }
     }
 
     @Override
@@ -34,11 +27,10 @@ public class WinGameState extends State {
 		graphics.fillRect(0, 0, Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
 		
 		graphics.setFont(new Font("Araial", Font.BOLD, 25));
-        for(int i=0;i<this.options.length;i++) {
-			if(i==1) graphics.setColor(Color.GREEN);
-			else graphics.setColor(Color.WHITE);
-			graphics.drawString(this.options[i], Game.WINDOW_WIDTH / 2 - 40, Game.WINDOW_HEIGHT /2 + 30*i);
-		}
+
+		graphics.setColor(Color.GREEN);
+
+		graphics.drawString(WIN_GAME, Game.WINDOW_WIDTH / 2 - 40, Game.WINDOW_HEIGHT /2);
     }
     
 }
