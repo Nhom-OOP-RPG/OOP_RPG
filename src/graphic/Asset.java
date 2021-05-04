6a46a2030bd7773a4fa25d7b3aa92dcb2e4f9690
longtruongv <57038442+longtruongv@users.noreply.github.com>
2021-05-04 08:50:12 +0700

    sua Assets

--- src/graphic/Asset.java
+++ src/graphic/Asset.java
@@ -10,10 +10,18 @@ public class Asset {
     //private static final int width = 40, height = 40;
     
     public static BufferedImage floor, grass, stone, wall, desertFloor, desertWall;
-    public static BufferedImage[][] player, playerDamaged;
+    public static BufferedImage[][] player;
+    public static BufferedImage[][][] playerDamaged;
     public static BufferedImage[] enemy1, enemy1Damaged;
 
     public static void init(){
+        initTile();
+        initPlayer();
+        initEnemy();
+        
+    }
+
+    private static void initTile(){
         //Tile
         floor = Utility.loadImage("res/texture/tile/floor.png");
         grass = Utility.loadImage("res/texture/tile/grass.png");
@@ -21,8 +29,10 @@ public class Asset {
         wall = Utility.loadImage("res/texture/tile/wall.png");
         desertFloor = Utility.loadImage("res/texture/tile/desertfloor.png");
         desertWall = Utility.loadImage("res/texture/tile/desertwall.png");
-
-        //Player
+    }
+    
+    private static void initPlayer(){
+        //Trang thai binh thuong
         player = new BufferedImage[4][2];
         
         player[0][0] = Utility.loadImage("res/texture/player/player.png");
@@ -37,20 +47,23 @@ public class Asset {
         player[3][0] = Utility.loadImage("res/texture/player/player_right.png");
         player[3][1] = Utility.loadImage("res/texture/player/player_right_2.png");
 
-        playerDamaged = new BufferedImage[4][2];
+        //Trang thai bi dau
+        playerDamaged = new BufferedImage[1][4][2];
         
-        playerDamaged[0][0] = Utility.loadImage("res/texture/player/player_damaged.png");
-        playerDamaged[0][1] = Utility.loadImage("res/texture/player/player_2_damaged.png");
+        playerDamaged[0][0][0] = Utility.loadImage("res/texture/player/player_damaged.png");
+        playerDamaged[0][0][1] = Utility.loadImage("res/texture/player/player_2_damaged.png");
 
-        playerDamaged[1][0] = Utility.loadImage("res/texture/player/player_back_damaged.png");
-        playerDamaged[1][1] = Utility.loadImage("res/texture/player/player_back_2_damaged.png");
+        playerDamaged[0][1][0] = Utility.loadImage("res/texture/player/player_back_damaged.png");
+        playerDamaged[0][1][1] = Utility.loadImage("res/texture/player/player_back_2_damaged.png");
         
-        playerDamaged[2][0] = Utility.loadImage("res/texture/player/player_left_damaged.png");
-        playerDamaged[2][1] = Utility.loadImage("res/texture/player/player_left_2_damaged.png");
+        playerDamaged[0][2][0] = Utility.loadImage("res/texture/player/player_left_damaged.png");
+        playerDamaged[0][2][1] = Utility.loadImage("res/texture/player/player_left_2_damaged.png");
 
-        playerDamaged[3][0] = Utility.loadImage("res/texture/player/player_right_damaged.png");
-        playerDamaged[3][1] = Utility.loadImage("res/texture/player/player_right_2_damaged.png");
+        playerDamaged[0][3][0] = Utility.loadImage("res/texture/player/player_right_damaged.png");
+        playerDamaged[0][3][1] = Utility.loadImage("res/texture/player/player_right_2_damaged.png");
+    }
 
+    private static void initEnemy(){
         //Enemy
         //tạm thời mới có 1 loại quái
         enemy1 = new BufferedImage[2];
@@ -60,5 +73,4 @@ public class Asset {
         enemy1Damaged[0] = Utility.loadImage("res/texture/enemy/enemy_damaged.png");
         enemy1Damaged[1] = Utility.loadImage("res/texture/enemy/enemy_2_damaged.png");
     }
-    
 }
