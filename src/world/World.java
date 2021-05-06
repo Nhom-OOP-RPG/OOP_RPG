e4c724049266432bb2567cfe4acf83ec811c23ec
longtruongv <57038442+longtruongv@users.noreply.github.com>
2021-05-06 23:45:53 +0700

    thử cơ chế 1.0

--- src/world/World.java
+++ src/world/World.java
@@ -29,7 +29,7 @@ public class World {
         init();
         initEasy();
 
-        currentRoom = worldMap[1][0];
+        currentRoom = worldMap[0][0];
     }
 
     //phương thức chuyển các phòng
