68c80ac0b00ad5f327392237827db3f800d2d03a
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-06-03 23:40:26 +0700

    Update PauseState.java

--- src/state/PauseState.java
+++ src/state/PauseState.java
@@ -1,6 +1,10 @@
 package state;
 
 import java.awt.Graphics;
+
+import graphic.Asset;
+import graphic.tile.Tile;
+
 import java.awt.Color;
 import java.awt.Font;
 
@@ -66,10 +70,15 @@ public class PauseState extends State {
 
     @Override
     public void render(Graphics graphics) {
-        graphics.setColor(new Color(30, 30, 70));
-		graphics.fillRect(0, 0, Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
-		
+
+		graphics.fillRect(0, 0, Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);		
 		graphics.setFont(new Font("Araial", Font.BOLD, 25));
+        for (int y = 0; y < 15; y++){
+            for (int x = 0; x < 20; x++){
+                Tile tile = new Tile(Asset.main2, 7);
+                tile.render(graphics, x, y);
+            }
+        }
 		for(int i=0;i<this.optionsMenu.length;i++) {
 			if(i==this.selected) graphics.setColor(Color.GREEN);
 			else graphics.setColor(Color.WHITE);
