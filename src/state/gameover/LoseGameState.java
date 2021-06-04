a53134290380b3c3519602b17aaccbafcc437dde
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-06-04 10:07:53 +0700

    Update LoseGameState.java

--- src/state/gameover/LoseGameState.java
+++ src/state/gameover/LoseGameState.java
@@ -1,6 +1,9 @@
 package state.gameover;
 
 import java.awt.Graphics;
+
+import graphic.Asset;
+
 import java.awt.Color;
 import java.awt.Font;
 
@@ -10,17 +13,14 @@ import state.State;
 
 public class LoseGameState extends State {
 
-    private static final String LOSE_GAME = "You Died!";
-    private static final String CONTINUE = "Press Enter to continue!";
-
     public LoseGameState(Handler handler) {
         super(handler);
     }
 
     @Override
     public void tick() {
-        if (handler.getKeyManager().enter) {
-            if (handler.getPlayer().getLives() > 0) {
+        if (handler.getPlayer().getLives() > 0) {
+            if (handler.getKeyManager().enter) {
                 handler.getPlayer().revive();
                 handler.getPlayer().setX(360f);
                 handler.getPlayer().setY(280f);
@@ -28,23 +28,22 @@ public class LoseGameState extends State {
                 State.setState(handler.getGame().getGameState());
                 return;
             }
-            State.setState(handler.getGame().getPlayAgainState());
-            keyPressedDelayCount = 0;
+                keyPressedDelayCount = 0;
         }
+        else handler.getGame().getPlayAgainState().tick();
     }
 
     @Override
     public void render(Graphics graphics) {
-
-        graphics.setColor(new Color(30, 30, 70));
-		graphics.fillRect(0, 0, Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
-		
-		graphics.setFont(new Font("Araial", Font.BOLD, 25));
-        graphics.setColor(Color.WHITE);
-        graphics.drawString(LOSE_GAME, Game.WINDOW_WIDTH / 2 - 100, Game.WINDOW_HEIGHT /2);
-        graphics.drawString("You have " + handler.getPlayer().getLives() + " lives left!", Game.WINDOW_WIDTH / 2 - 100, Game.WINDOW_HEIGHT /2 + 30);
-        graphics.setColor(Color.GREEN);
-        graphics.drawString(CONTINUE, Game.WINDOW_WIDTH / 2 - 100, Game.WINDOW_HEIGHT /2 + 60);
+        if(handler.getPlayer().getLives() > 0){
+            graphics.drawImage(Asset.youDied,0,0, 20*40, 15*40, null);
+            graphics.setFont(new Font("Copperplate Gothic Bold", Font.BOLD, 30));
+            graphics.setColor(Color.YELLOW);
+            graphics.drawString("You have " + handler.getPlayer().getLives() + " lives left!", Game.WINDOW_WIDTH / 2 - 200, Game.WINDOW_HEIGHT /2 + 30);
+        }
+        else{
+            handler.getGame().getPlayAgainState().render(graphics);
+        }
     }
 }
 
